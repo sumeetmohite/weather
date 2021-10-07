5b97a899c965dc3ebd6873b3d9ea2e8e9f5edb36
sumeetmohite <sumeetmohite9@gmail.com>
2021-10-07 12:13:35 +0530

    added right sidebar

--- src/components/SecondRow/SecondRow.js
+++ src/components/SecondRow/SecondRow.js
@@ -1,13 +1,10 @@
 import Weather from "../Weather/Weather"
-
+import Loader from "react-loader-spinner";
 
 const SecondRow = ({weather,date}) => {
   return (
     <div className='second-row'>
-      { weather.main && (
-          <Weather date={date} weather={weather}/>
-      )
-      }
+      {weather.main && <Weather date={date} weather={weather}/>}
     </div>
   )
 }
